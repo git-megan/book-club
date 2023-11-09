e7f77cb3bc0a5a29756a758ebcef9405c6b54e17
Megan Brown <uxtechnologist@gmail.com>
2023-11-09 10:41:17 -0800

    add ability to shuffle speaker order

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
+// function for getting a random integer between min and max
+function getRandomInt(min, max) {
+	// Math.random() gets a decimal between 0 and 1 inclusive
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function for creating a random order
+function getNewOrder() {
+	let previousList = ["Julie", "Josh", "Megan"];
+	let newOrder = [];
+
+	while (previousList.length > 1) {
+		// get an index for a name in the previous list of names
+		let index = getRandomInt(0, previousList.length - 1);
+
+		// put that name into the new order, and remove it from the previous list of names
+		newOrder.push(previousList[index]);
+		previousList.splice(index, 1);
+	}
+
+	// put that last remaining name into the new order, and remove it from the previous list of names
+	newOrder.push(previousList.shift());
+
+	// return the new order of names
+	return newOrder;
+}
+
 // identify the shuffle button and listen for a click
 let shuffleButton = document.getElementById("shuffle-button");
 shuffleButton.addEventListener("click", function () {
@@ -5,7 +32,12 @@ shuffleButton.addEventListener("click", function () {
 	let firstPerson = document.getElementById("first");
 	let secondPerson = document.getElementById("second");
 	let thirdPerson = document.getElementById("third");
-	firstPerson.textContent = "Mango A Gogo";
-	secondPerson.textContent = "🍓 Strawbery Whirl";
-	thirdPerson.textContent = "Orange Dream Machine";
+
+	// get a new speaker order
+	let newSpeakerOrder = getNewOrder();
+
+	// update the text content displayed in the HTML
+	firstPerson.textContent = newSpeakerOrder[0];
+	secondPerson.textContent = newSpeakerOrder[1];
+	thirdPerson.textContent = newSpeakerOrder[2];
 });
